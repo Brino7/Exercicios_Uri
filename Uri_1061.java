558330ff56edb570a233cab9b31acbe744e658e3
Anderson Silva Brino <brino7@hotmail.com>
2017-11-22 17:03:21 -0200

    inicionodo a resolução do problema do tempo do evento.

---
+++ Uri_1061.java
@@ -0,0 +1,18 @@
+
+package exerciciosuri;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @autor Anderson Silva Brino 
+ * @data   22/11/2017
+ * @horario   15:44:38
+ */
+public class Uri_1061 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int diaIn, diaFi, horaIn, horaFin, minIn, minFin, segIn, segFin;
+        
+    }
+}
