d9e2a7d548c7779442493382a9d8c0ad77bb6403
Anderson Silva Brino <brino7@hotmail.com>
2017-12-04 23:15:56 -0200

    as telas de requisções dos tempos de um evento.

--- Uri_1061.java
+++ Uri_1061.java
@@ -2,6 +2,7 @@
 package exerciciosuri;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  * 
@@ -13,6 +14,15 @@ public class Uri_1061 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int diaIn, diaFi, horaIn, horaFin, minIn, minFin, segIn, segFin;
-        diaIn = scan.nextInt();
+        diaIn = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o dia inicial do evento."));
+        horaIn = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite a hora inicial do evento."));
+        minIn = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite os minutos iniciais do evento."));
+        segIn = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite os segundos iniciais do evento."));
+        
+        diaFi = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o dia final do evento."));
+        horaFin = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite a hora final do evento."));
+        minFin = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite os minutos finais do evento."));
+        segFin = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite os segunds finais do evento."));
+        
     }
 }
