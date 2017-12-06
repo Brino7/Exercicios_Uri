617f9e6a28a63c8767ae9229f7addfd201d7d5f5
Anderson Silva Brino <brino7@hotmail.com>
2017-12-06 12:05:07 -0200

    alterações e adições do tratamento de erro e de validação dos dias de inicio e fim do evento.

--- Uri_1061.java
+++ Uri_1061.java
@@ -1,28 +1,38 @@
-
 package exerciciosuri;
 
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 /**
- * 
- * @autor Anderson Silva Brino 
- * @data   22/11/2017
- * @horario   15:44:38
+ *
+ * @autor Anderson Silva Brino
+ * @data 22/11/2017
+ * @horario 15:44:38
  */
 public class Uri_1061 {
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int diaIn, diaFi, horaIn, horaFin, minIn, minFin, segIn, segFin;
-        diaIn = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o dia inicial do evento."));
-        horaIn = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite a hora inicial do evento."));
-        minIn = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite os minutos iniciais do evento."));
-        segIn = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite os segundos iniciais do evento."));
-        
-        diaFi = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o dia final do evento."));
-        horaFin = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite a hora final do evento."));
-        minFin = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite os minutos finais do evento."));
-        segFin = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite os segunds finais do evento."));
-        
+        int diaIn = 0, diaFi = 0, horaIn, horaFin, minIn, minFin, segIn, segFin;
+        try {
+            diaIn = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o dia inicial do evento."));
+            horaIn = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a hora inicial do evento."));
+            minIn = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite os minutos iniciais do evento."));
+            segIn = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite os segundos iniciais do evento."));
+
+            diaFi = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o dia final do evento."));
+            horaFin = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a hora final do evento."));
+            minFin = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite os minutos finais do evento."));
+            segFin = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite os segunds finais do evento."));
+        } catch (NumberFormatException er) {
+            JOptionPane.showMessageDialog(null, "Numero em formato inválido.");
+        }
+        if (diaIn == diaFi) {
+            int diasSeg = diaIn * (24 * 3600);
+        } else {
+            int diasSeg;
+            diasSeg = ((diaFi * (24 * 3600)) - ((24 * 3600) * diaIn));
+        }
+
     }
 }
